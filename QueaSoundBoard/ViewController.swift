c094ef5f02082a2384216ac4074e0fe5985b1f95
Ruben Freddy Quea Jacho <qjcoders@gmail.com>
2023-10-31 10:53:00 -0500

    Proyecto completo, se agrego reproduccion en tableview y eliminacion con editingStyle

--- QueaSoundBoard/ViewController.swift
+++ QueaSoundBoard/ViewController.swift
@@ -6,14 +6,75 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, AVAudioPlayerDelegate{
+    
 
+    @IBOutlet weak var tablaGrabaciones: UITableView!
+    
+    var grabaciones:[Grabacion] = []
+    var reproducirAudio:AVAudioPlayer?
+    var nameGrabacion:String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tablaGrabaciones.dataSource = self
+        tablaGrabaciones.delegate = self
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do {
+            grabaciones = try context.fetch(Grabacion.fetchRequest())
+            tablaGrabaciones.reloadData()
+        } catch  {
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return grabaciones.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let grabacion = grabaciones[indexPath.row]
+        cell.textLabel?.text = grabacion.nombre
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let grabacion = grabaciones[indexPath.row]
+        do {
+            reproducirAudio = try AVAudioPlayer(data: grabacion.audio! as Data)
+            reproducirAudio?.delegate = self
+            reproducirAudio?.play()
+            nameGrabacion = grabacion.nombre!
+            print("Reproduciendo \(nameGrabacion!)")
+        } catch {
+            print("Error al reproducir el audio: \(error.localizedDescription)")
+        }
+        tablaGrabaciones.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let grabacion = grabaciones[indexPath.row]
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(grabacion)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            do {
+                grabaciones = try context.fetch(Grabacion.fetchRequest())
+                tablaGrabaciones.reloadData()
+            }catch  {}
+        }
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print("Pausa \(nameGrabacion!)")
+    }
 }
 
